a2112dff0f64ff708abbc965bdf4cc11a780cad0
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-02-23 16:28:58 +0300

    routes

--- index.js
+++ index.js
@@ -28,3 +28,34 @@ app.get('/', function(request, response) {
     // Render login template
     response.sendFile(path.join(__dirname + '/login.html'));
 });
+
+
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+    // Capture the input fields
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exists and are not empty
+    if (username && password) {
+        // Execute SQL query that'll select the account from the database based on the specified username and password
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            // If there is an issue with the query, output the error
+            if (error) throw error;
+            // If the account exists
+            if (results.length > 0) {
+                // Authenticate the user
+                request.session.loggedin = true;
+                request.session.username = username;
+                // Redirect to home page
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
