d8936e2c6244d65a1a2a3652f3fb1ce5a54e74fc
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-02-23 16:25:36 +0300

    connection

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodelogin'
+});
+
+const app = express();
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
