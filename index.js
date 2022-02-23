9b5fa60a40963526a7a46e858b9fafe26b72dc86
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-02-23 16:30:05 +0300

    welcome tohome

--- index.js
+++ index.js
@@ -59,3 +59,17 @@ app.post('/auth', function(request, response) {
         response.end();
     }
 });
+
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+    // If the user is loggedin
+    if (request.session.loggedin) {
+        // Output username
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+        // Not logged in
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
